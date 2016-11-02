9cc0b190e59d1437c7daa80c3f62296da0060fa1
sancwk <wakschamara@gmail.com>
2016-11-02 21:46:22 +0530

    * layout rearranged * added spinner to kotNO * made it handle multiple KOTs * added auto adjusting the popup window height * added button actions * added newBill activity and redirected to it

--- app/src/main/java/com/ites/pos/Activities/rooms/Room2.java
+++ app/src/main/java/com/ites/pos/Activities/rooms/Room2.java
@@ -1,26 +1,28 @@
 package com.ites.pos.Activities.rooms;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ites.pos.Activities.NewBill;
 import com.ites.pos.BillAdapter;
 import com.ites.pos.Controllers.NetworkController;
 import com.ites.pos.Controllers.ResponseCallBack;
@@ -31,6 +33,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,8 +45,10 @@ public class Room2 extends Fragment {
     private JSONArray tableConfigs;
     private PopupWindow orderInfo;
     private ImageButton closeBtn;
-    private TextView tableHeader,guestNoD,roomNoD,kotNoD,timeStampD,usernameD,kotTotalD;
+    private TextView tableHeader, guestNoD, roomNoD, timeStampD, usernameD, kotTotalD;
+    private Spinner kotNoD;
     private RecyclerView billItems;
+    private Button newBtn, editBtn, guestBtn, tableCloseBtn;
 
     public Room2() {
         // Required empty public constructor
@@ -67,18 +72,18 @@ public class Room2 extends Fragment {
         }
 
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.room2, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_room2, container, false);
 
         final GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
 
-        Room2.TableAdapter tableAdapter = new Room2.TableAdapter(getContext());
+        TableAdapter tableAdapter = new TableAdapter(getContext());
 
         gridview.setAdapter(tableAdapter);
 
         gridview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                String tableH=null,tableId=null;
+                String tableH = null, tableId = null;
                 NetworkController netCtrl;
 
                 netCtrl = new NetworkController(getContext());
@@ -88,11 +93,15 @@ public class Room2 extends Fragment {
                 tableHeader = (TextView) orderInfoDisplay.findViewById(R.id.tableHeader);
                 guestNoD = (TextView) orderInfoDisplay.findViewById(R.id.guestVal);
                 roomNoD = (TextView) orderInfoDisplay.findViewById(R.id.roomNoVal);
-                kotNoD = (TextView) orderInfoDisplay.findViewById(R.id.kotNoVal);
+                kotNoD = (Spinner) orderInfoDisplay.findViewById(R.id.kotNoVal);
                 timeStampD = (TextView) orderInfoDisplay.findViewById(R.id.timeStampVal);
                 usernameD = (TextView) orderInfoDisplay.findViewById(R.id.userVal);
                 billItems = (RecyclerView) orderInfoDisplay.findViewById(R.id.billItems);
                 kotTotalD = (TextView) orderInfoDisplay.findViewById(R.id.kotTotal);
+                newBtn = (Button) orderInfoDisplay.findViewById(R.id.newBtn);
+                editBtn = (Button) orderInfoDisplay.findViewById(R.id.editBtn);
+                guestBtn = (Button) orderInfoDisplay.findViewById(R.id.guestBtn);
+                tableCloseBtn = (Button) orderInfoDisplay.findViewById(R.id.tableCloseBtn);
 
                 RecyclerView.LayoutManager mgr = new LinearLayoutManager(getContext());
                 billItems.setLayoutManager(mgr);
@@ -100,8 +109,7 @@ public class Room2 extends Fragment {
                 try {
                     JSONObject tmp = tableConfigs.getJSONObject(position);
                     tableH = tmp.getString("tableName");
-                    tableId = tmp.getString("tableNo");
-                    Log.d("Table ID", tableId);
+                    tableId = tmp.getString("table_Id");
                 } catch (JSONException ex) {
                     ex.printStackTrace();
                 }
@@ -109,6 +117,7 @@ public class Room2 extends Fragment {
                 tableHeader.setText(tableH);
 
                 final String finalTableH = tableH;
+                final String finalTableId = tableId;
                 netCtrl.getOpenTableDetails(tableId, new ResponseCallBack() {
                     @Override
                     public void gotTableConfigs(String data) {
@@ -116,35 +125,118 @@ public class Room2 extends Fragment {
 
                     @Override
                     public void gotOpenTableDetails(String data) {
-                        List<OrderBillItem> billItemsList = new ArrayList<OrderBillItem>();
+                        final List<OrderBillItem> billItemsListAll = new ArrayList<>();
+                        final List<OrderBillItem> billItemsList = new ArrayList<>();
+                        final List<String> KOTNums = new ArrayList<>();
 
-                        try{
+                        try {
                             JSONArray oderBillItems = new JSONArray(data);
 
-                            for(int i = 0; i<oderBillItems.length(); i++){
+                            for (int i = 0; i < oderBillItems.length(); i++) {
                                 JSONObject tmp = oderBillItems.getJSONObject(i);
                                 tmp.put("tableName", finalTableH);
 
                                 OrderBillItem billItem = new OrderBillItem(tmp);
 
-                                billItemsList.add(billItem);
+                                billItemsListAll.add(billItem);
                             }
 
-                            // set the common values in the UI
-                            guestNoD.setText(billItemsList.get(0).getGuestNo());
-                            roomNoD.setText(billItemsList.get(0).getRoomNo());
-                            kotNoD.setText(billItemsList.get(0).getKotNo());
-                            timeStampD.setText(billItemsList.get(0).getSystemDate());
-                            usernameD.setText(billItemsList.get(0).getUserName());
-                        }catch (JSONException ex){
+                            // split the billItemsList
+                            for (int i = 0; i < billItemsListAll.size(); i++) {
+                                if (!KOTNums.contains(billItemsListAll.get(i).getKotNo())) {
+                                    KOTNums.add(billItemsListAll.get(i).getKotNo());
+                                }
+                            }
+
+                            // set KOT No spinner
+                            ArrayAdapter<String> kotAdapter = new ArrayAdapter<>(getContext(), R.layout.kot_spinner_item, KOTNums);
+                            kotAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            kotNoD.setAdapter(kotAdapter);
+
+                            kotNoD.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                    billItemsList.clear();
+                                    String KotNo = KOTNums.get(position);
+
+                                    for (int i = 0; i < billItemsListAll.size(); i++) {
+
+                                        if (billItemsListAll.get(i).getKotNo().equals(KotNo)) {
+                                            billItemsList.add(billItemsListAll.get(i));
+                                        }
+
+                                    }
+
+                                    // date format convert
+                                    Timestamp stamp = new Timestamp(Long.valueOf(billItemsList.get(0).getSystemDate()));
+
+                                    // set the common values in the UI
+                                    guestNoD.setText(billItemsList.get(0).getGuestNo());
+                                    roomNoD.setText(billItemsList.get(0).getRoomNo());
+                                    timeStampD.setText(stamp.toString());
+                                    usernameD.setText(billItemsList.get(0).getUserName());
+
+                                    RecyclerView.Adapter adapter = new BillAdapter(billItemsList);
+
+                                    // set parameters
+                                    ViewGroup.LayoutParams params = billItems.getLayoutParams();
+
+                                    if (billItemsList.size() > 7) {
+                                        params.height = 310;
+                                    } else {
+                                        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                                    }
+
+                                    billItems.setLayoutParams(params);
+                                    billItems.setAdapter(adapter);
+
+                                    kotTotalD.setText(Float.toString(calculateKotTotal(billItemsList)));
+
+                                    // button action listeners
+                                    newBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // go to new bill activity
+                                            Intent nxt = new Intent(getActivity(), NewBill.class);
+                                            nxt.putExtra("tableId", finalTableId);
+                                            startActivity(nxt);
+                                        }
+                                    });
+
+                                    editBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // go to edit bill activity
+                                            Toast.makeText(getContext(), "Edit Order", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+
+                                    guestBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // generate guest bill
+                                            Toast.makeText(getContext(), "Guest Bill", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+
+                                    tableCloseBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // close table
+                                            Toast.makeText(getContext(), "Close Table", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+                                    // skip
+                                }
+                            });
+
+                        } catch (JSONException ex) {
                             ex.printStackTrace();
                         }
-
-                        RecyclerView.Adapter adapter = new BillAdapter(billItemsList);
-
-                        billItems.setAdapter(adapter);
-
-                        kotTotalD.setText(Float.toString(calculateKotTotal(billItemsList)));
                     }
                 });
 
@@ -170,15 +262,11 @@ public class Room2 extends Fragment {
         return rootView;
     }
 
-    public float calculateKotTotal(List<OrderBillItem> list){
-        float total=0;
+    public float calculateKotTotal(List<OrderBillItem> list) {
+        float total = 0;
 
-        Log.d("Lsit >>",list.toString());
-
-        for(int i = 0; i<list.size(); i++){
-            total = total + Float.valueOf(list.get(i).getQty())*Float.valueOf(list.get(i).getUnitPrice());
-
-            Log.d("Current Total", Float.valueOf(total)+"");
+        for (int i = 0; i < list.size(); i++) {
+            total = total + Float.valueOf(list.get(i).getQty()) * Float.valueOf(list.get(i).getUnitPrice());
         }
 
         return total;
@@ -209,10 +297,10 @@ public class Room2 extends Fragment {
         public View getView(int position, View convertView, ViewGroup parent) {
 
             String tableName = null;
-            try{
+            try {
                 JSONObject tmp = tableConfigs.getJSONObject(position);
                 tableName = tmp.getString("tableName");
-            }catch (JSONException ex){
+            } catch (JSONException ex) {
                 ex.printStackTrace();
             }
 
--- app/src/main/java/com/ites/pos/Activities/rooms/Room4.java
+++ app/src/main/java/com/ites/pos/Activities/rooms/Room4.java
@@ -1,25 +1,28 @@
 package com.ites.pos.Activities.rooms;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.ites.pos.Activities.NewBill;
 import com.ites.pos.BillAdapter;
 import com.ites.pos.Controllers.NetworkController;
 import com.ites.pos.Controllers.ResponseCallBack;
@@ -30,6 +33,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,8 +45,10 @@ public class Room4 extends Fragment {
     private JSONArray tableConfigs;
     private PopupWindow orderInfo;
     private ImageButton closeBtn;
-    private TextView tableHeader,guestNoD,roomNoD,kotNoD,timeStampD,usernameD,kotTotalD;
+    private TextView tableHeader, guestNoD, roomNoD, timeStampD, usernameD, kotTotalD;
+    private Spinner kotNoD;
     private RecyclerView billItems;
+    private Button newBtn, editBtn, guestBtn, tableCloseBtn;
 
     public Room4() {
         // Required empty public constructor
@@ -66,7 +72,7 @@ public class Room4 extends Fragment {
         }
 
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.room4, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_room4, container, false);
 
         final GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
 
@@ -77,7 +83,7 @@ public class Room4 extends Fragment {
         gridview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                String tableH=null,tableId=null;
+                String tableH = null, tableId = null;
                 NetworkController netCtrl;
 
                 netCtrl = new NetworkController(getContext());
@@ -87,11 +93,15 @@ public class Room4 extends Fragment {
                 tableHeader = (TextView) orderInfoDisplay.findViewById(R.id.tableHeader);
                 guestNoD = (TextView) orderInfoDisplay.findViewById(R.id.guestVal);
                 roomNoD = (TextView) orderInfoDisplay.findViewById(R.id.roomNoVal);
-                kotNoD = (TextView) orderInfoDisplay.findViewById(R.id.kotNoVal);
+                kotNoD = (Spinner) orderInfoDisplay.findViewById(R.id.kotNoVal);
                 timeStampD = (TextView) orderInfoDisplay.findViewById(R.id.timeStampVal);
                 usernameD = (TextView) orderInfoDisplay.findViewById(R.id.userVal);
                 billItems = (RecyclerView) orderInfoDisplay.findViewById(R.id.billItems);
                 kotTotalD = (TextView) orderInfoDisplay.findViewById(R.id.kotTotal);
+                newBtn = (Button) orderInfoDisplay.findViewById(R.id.newBtn);
+                editBtn = (Button) orderInfoDisplay.findViewById(R.id.editBtn);
+                guestBtn = (Button) orderInfoDisplay.findViewById(R.id.guestBtn);
+                tableCloseBtn = (Button) orderInfoDisplay.findViewById(R.id.tableCloseBtn);
 
                 RecyclerView.LayoutManager mgr = new LinearLayoutManager(getContext());
                 billItems.setLayoutManager(mgr);
@@ -99,7 +109,7 @@ public class Room4 extends Fragment {
                 try {
                     JSONObject tmp = tableConfigs.getJSONObject(position);
                     tableH = tmp.getString("tableName");
-                    tableId = tmp.getString("tableId");
+                    tableId = tmp.getString("table_Id");
                 } catch (JSONException ex) {
                     ex.printStackTrace();
                 }
@@ -107,6 +117,7 @@ public class Room4 extends Fragment {
                 tableHeader.setText(tableH);
 
                 final String finalTableH = tableH;
+                final String finalTableId = tableId;
                 netCtrl.getOpenTableDetails(tableId, new ResponseCallBack() {
                     @Override
                     public void gotTableConfigs(String data) {
@@ -114,39 +125,121 @@ public class Room4 extends Fragment {
 
                     @Override
                     public void gotOpenTableDetails(String data) {
-                        List<OrderBillItem> billItemsList = new ArrayList<OrderBillItem>();
+                        final List<OrderBillItem> billItemsListAll = new ArrayList<>();
+                        final List<OrderBillItem> billItemsList = new ArrayList<>();
+                        final List<String> KOTNums = new ArrayList<>();
 
-                        try{
+                        try {
                             JSONArray oderBillItems = new JSONArray(data);
 
-                            for(int i = 0; i<oderBillItems.length(); i++){
+                            for (int i = 0; i < oderBillItems.length(); i++) {
                                 JSONObject tmp = oderBillItems.getJSONObject(i);
                                 tmp.put("tableName", finalTableH);
 
                                 OrderBillItem billItem = new OrderBillItem(tmp);
 
-                                billItemsList.add(billItem);
+                                billItemsListAll.add(billItem);
                             }
 
-                            // set the common values in the UI
-                            guestNoD.setText(billItemsList.get(0).getGuestNo());
-                            roomNoD.setText(billItemsList.get(0).getRoomNo());
-                            kotNoD.setText(billItemsList.get(0).getKotNo());
-                            timeStampD.setText(billItemsList.get(0).getSystemDate());
-                            usernameD.setText(billItemsList.get(0).getUserName());
-                        }catch (JSONException ex){
+                            // split the billItemsList
+                            for (int i = 0; i < billItemsListAll.size(); i++) {
+                                if (!KOTNums.contains(billItemsListAll.get(i).getKotNo())) {
+                                    KOTNums.add(billItemsListAll.get(i).getKotNo());
+                                }
+                            }
+
+                            // set KOT No spinner
+                            ArrayAdapter<String> kotAdapter = new ArrayAdapter<>(getContext(), R.layout.kot_spinner_item, KOTNums);
+                            kotAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            kotNoD.setAdapter(kotAdapter);
+
+                            kotNoD.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                    billItemsList.clear();
+                                    String KotNo = KOTNums.get(position);
+
+                                    for (int i = 0; i < billItemsListAll.size(); i++) {
+
+                                        if (billItemsListAll.get(i).getKotNo().equals(KotNo)) {
+                                            billItemsList.add(billItemsListAll.get(i));
+                                        }
+                                    }
+
+                                    // date format convert
+                                    Timestamp stamp = new Timestamp(Long.valueOf(billItemsList.get(0).getSystemDate()));
+
+                                    // set the common values in the UI
+                                    guestNoD.setText(billItemsList.get(0).getGuestNo());
+                                    roomNoD.setText(billItemsList.get(0).getRoomNo());
+                                    timeStampD.setText(stamp.toString());
+                                    usernameD.setText(billItemsList.get(0).getUserName());
+
+                                    RecyclerView.Adapter adapter = new BillAdapter(billItemsList);
+
+                                    // set parameters
+                                    ViewGroup.LayoutParams params = billItems.getLayoutParams();
+
+                                    if (billItemsList.size() > 7) {
+                                        params.height = 310;
+                                    } else {
+                                        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                                    }
+
+                                    billItems.setLayoutParams(params);
+                                    billItems.setAdapter(adapter);
+
+                                    kotTotalD.setText(Float.toString(calculateKotTotal(billItemsList)));
+
+                                    // button action listeners
+                                    newBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // go to new bill activity
+                                            Intent nxt = new Intent(getActivity(), NewBill.class);
+                                            nxt.putExtra("tableId", finalTableId);
+                                            startActivity(nxt);
+                                        }
+                                    });
+
+                                    editBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // go to edit bill activity
+                                            Toast.makeText(getContext(), "Edit Order", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+
+                                    guestBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // generate guest bill
+                                            Toast.makeText(getContext(), "Guest Bill", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+
+                                    tableCloseBtn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            // close table
+                                            Toast.makeText(getContext(), "Close Table", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+                                    // skip
+                                }
+                            });
+
+                        } catch (JSONException ex) {
                             ex.printStackTrace();
                         }
-
-                        RecyclerView.Adapter adapter = new BillAdapter(billItemsList);
-
-                        billItems.setAdapter(adapter);
-
-                        kotTotalD.setText(Float.toString(calculateKotTotal(billItemsList)));
                     }
                 });
 
-                orderInfo = new PopupWindow(orderInfoDisplay, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+                orderInfo = new PopupWindow(orderInfoDisplay, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
                 if (Build.VERSION.SDK_INT >= 21) {
                     orderInfo.setElevation(5.0f);
@@ -168,15 +261,11 @@ public class Room4 extends Fragment {
         return rootView;
     }
 
-    public float calculateKotTotal(List<OrderBillItem> list){
-        float total=0;
+    public float calculateKotTotal(List<OrderBillItem> list) {
+        float total = 0;
 
-        Log.d("Lsit >>",list.toString());
-
-        for(int i = 0; i<list.size(); i++){
-            total = total + Float.valueOf(list.get(i).getQty())*Float.valueOf(list.get(i).getUnitPrice());
-
-            Log.d("Current Total", Float.valueOf(total)+"");
+        for (int i = 0; i < list.size(); i++) {
+            total = total + Float.valueOf(list.get(i).getQty()) * Float.valueOf(list.get(i).getUnitPrice());
         }
 
         return total;
