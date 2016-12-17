fdaee2b5af2686db10fac0242ccea0d2f1cabcb9
sancwk <wakschamara@gmail.com>
2016-12-17 23:40:04 +0530

    made disable button soon after they taped

--- app/src/main/java/com/ites/pos/Activities/fragments/SelectGuestFragment.java
+++ app/src/main/java/com/ites/pos/Activities/fragments/SelectGuestFragment.java
@@ -1,12 +1,15 @@
 package com.ites.pos.Activities.fragments;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,12 +20,14 @@ import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ites.pos.Activities.ItemPunching;
 import com.ites.pos.GuestFragmentType;
 import com.ites.pos.ManagerListAdapter;
 import com.ites.pos.Models.HouseAccount;
 import com.ites.pos.Models.ReservationRoom;
 import com.ites.pos.NetworkController;
 import com.ites.pos.ReservRoomAdapter;
+import com.ites.pos.ResponseCallBack;
 import com.ites.pos.main_activity.R;
 
 import org.json.JSONArray;
@@ -37,6 +42,8 @@ import java.util.List;
  */
 
 public class SelectGuestFragment extends Fragment {
+    private static final String SNACK_TEXT_COLOR = "#387ef4";
+
     private int fragmentType;
     private String restaurantId, waiterId, mealId, restRoomId, tableId;
     private RecyclerView.Adapter adapter;
@@ -80,18 +87,63 @@ public class SelectGuestFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if (fragmentType == GuestFragmentType.WALK_IN) {
-                    if (adultCount.getValue() == 0) {
-                        Snackbar snackbar = Snackbar.make(fragmentView, "You haven't set the Guest Adults Count!", Snackbar.LENGTH_SHORT);
-                        if (Build.VERSION.SDK_INT >= 17) {
-                            snackbar.getView().findViewById(android.support.design.R.id.snackbar_text).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                        } else {
-                            ((TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text)).setGravity(Gravity.CENTER);
+                    // make sure that button action is synchronized
+                    cntBtn.setEnabled(false);
+                    cntBtn.setBackgroundColor(Color.GRAY);
+
+                    // network call
+                    netCtrl.sendGuestDetails(getGenericObject(getSelectedItem()), new ResponseCallBack() {
+                        @Override
+                        public void gotAllUsers(String data) {
+
                         }
-                        snackbar.show();
+
+                        @Override
+                        public void gotUserAuth(String data) {
+
+                        }
+
+                        @Override
+                        public void gotTableConfigs(String data) {
+
+                        }
+
+                        @Override
+                        public void gotOpenTableDetails(String data) {
+
+                        }
+
+                        @Override
+                        public void gotReservationRoomList(String data) {
+
+                        }
+
+                        @Override
+                        public void gotHouseAccList(String data) {
+
+                        }
+
+                        @Override
+                        public void gotRestaurantItems(String data) {
+                            // redirect to itemPunching
+                            Intent nxt = new Intent(getContext(), ItemPunching.class);
+                            nxt.putExtra("restaurantItems", data);
+                            startActivity(nxt);
+                            getActivity().finish();
+                        }
+                    });
+
+                    // notification
+                    Snackbar snackbar = Snackbar.make(fragmentView, "Guest Details sent!", Snackbar.LENGTH_SHORT);
+                    TextView tv = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+                    tv.setTextColor(Color.parseColor(SNACK_TEXT_COLOR));
+                    if (Build.VERSION.SDK_INT >= 17) {
+                        snackbar.getView().findViewById(android.support.design.R.id.snackbar_text).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
                     } else {
-                        netCtrl.sendGuestDetails(getGenericObject(getSelectedItem()));
-                        Toast.makeText(getContext(), "Guest details sent!", Toast.LENGTH_SHORT).show();
+                        tv.setGravity(Gravity.CENTER);
                     }
+                    snackbar.show();
+
                 } else if (fragmentType == GuestFragmentType.MANAGER_LIST) {
                     proceedButtonClick(netCtrl, fragmentView, "You haven't set a Manager House Account!");
                 }
@@ -167,7 +219,7 @@ public class SelectGuestFragment extends Fragment {
         }
 
         // setup number pickers
-        adultCount.setMinValue(0);
+        adultCount.setMinValue(1);
         adultCount.setMaxValue(20);
         adultCount.setWrapSelectorWheel(true);
         adultCount.canScrollHorizontally(View.SCROLL_INDICATOR_RIGHT);
@@ -195,19 +247,77 @@ public class SelectGuestFragment extends Fragment {
 
     private void proceedButtonClick(NetworkController netCtrl, View fragmentView, String msg) {
         if (getSelectedItem() != null) {
-            if (adultCount.getValue() != 0) {
-                netCtrl.sendGuestDetails(getGenericObject(getSelectedItem()));
-                Toast.makeText(getContext(), "Guest details sent!", Toast.LENGTH_SHORT).show();
-            } else {
-                Snackbar snackbar = Snackbar.make(fragmentView, "You haven't set the Guest Adults Count!", Snackbar.LENGTH_SHORT);
-                if (Build.VERSION.SDK_INT >= 17) {
-                    snackbar.getView().findViewById(android.support.design.R.id.snackbar_text).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                } else {
-                    ((TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text)).setGravity(Gravity.CENTER);
-                }
-                snackbar.show();
+            // make sure that button action is synchronized
+            if(fragmentType == GuestFragmentType.MANAGER_LIST){
+                cntBtn.setEnabled(false);
+                cntBtn.setBackgroundColor(Color.GRAY);
+            }else if(fragmentType == GuestFragmentType.ROOM_LIST){
+                basicBtn.setEnabled(false);
+                basicBtn.setBackgroundColor(Color.GRAY);
+                complBtn.setEnabled(false);
+                complBtn.setBackgroundColor(Color.GRAY);
+                extraBtn.setEnabled(false);
+                extraBtn.setBackgroundColor(Color.GRAY);
             }
+
+            // network call
+            netCtrl.sendGuestDetails(getGenericObject(getSelectedItem()), new ResponseCallBack() {
+                @Override
+                public void gotAllUsers(String data) {
+
+                }
+
+                @Override
+                public void gotUserAuth(String data) {
+
+                }
+
+                @Override
+                public void gotTableConfigs(String data) {
+
+                }
+
+                @Override
+                public void gotOpenTableDetails(String data) {
+
+                }
+
+                @Override
+                public void gotReservationRoomList(String data) {
+
+                }
+
+                @Override
+                public void gotHouseAccList(String data) {
+
+                }
+
+                @Override
+                public void gotRestaurantItems(String data) {
+                    // redirect to itemPunching
+                    Intent nxt = new Intent(getContext(), ItemPunching.class);
+                    nxt.putExtra("restaurantItems", data);
+                    startActivity(nxt);
+                    getActivity().finish();
+                }
+            });
+
+            // notification
+            Snackbar snackbar = Snackbar.make(fragmentView, "Guest Details sent!", Snackbar.LENGTH_SHORT);
+            TextView tv = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+            tv.setTextColor(Color.parseColor(SNACK_TEXT_COLOR));
+            if (Build.VERSION.SDK_INT >= 17) {
+                snackbar.getView().findViewById(android.support.design.R.id.snackbar_text).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            } else {
+                tv.setGravity(Gravity.CENTER);
+            }
+            snackbar.show();
+
+            // redirect to itemPunching
+
+
         } else {
+            // validation failed notification
             Snackbar snackbar = Snackbar.make(fragmentView, msg, Snackbar.LENGTH_SHORT);
             if (Build.VERSION.SDK_INT >= 17) {
                 snackbar.getView().findViewById(android.support.design.R.id.snackbar_text).setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
@@ -230,9 +340,10 @@ public class SelectGuestFragment extends Fragment {
             genericObj.put("guestLname", ((selected != null) && (fragmentType == GuestFragmentType.ROOM_LIST)) ? ((ReservationRoom) selected).getlName() : "");
             genericObj.put("adultsCount", adultCount.getValue());
             genericObj.put("kidsCount", kidsCount.getValue());
-            genericObj.put("confNo", ((selected != null) && (fragmentType == GuestFragmentType.ROOM_LIST)) ? ((ReservationRoom) selected).getConfNo() : "");
+            genericObj.put("confNo", ((selected != null) && (fragmentType == GuestFragmentType.ROOM_LIST)) ? ((ReservationRoom) selected).getConfNo() : "0");
+            genericObj.put("reservRoomNo", ((selected != null) && (fragmentType == GuestFragmentType.ROOM_LIST)) ? ((ReservationRoom) selected).getRoomNo() : "");
             genericObj.put("foodPackage", ((selected != null) && (fragmentType == GuestFragmentType.ROOM_LIST)) ? ((ReservationRoom) selected).getPakage() : "");
-            genericObj.put("hAccountId", ((selected != null) && (fragmentType == GuestFragmentType.MANAGER_LIST)) ? ((HouseAccount) selected).gethAccountId() : "");
+            genericObj.put("hAccountId", ((selected != null) && (fragmentType == GuestFragmentType.MANAGER_LIST)) ? ((HouseAccount) selected).gethAccountId() : "0");
         } catch (JSONException e) {
             e.printStackTrace();
         }
